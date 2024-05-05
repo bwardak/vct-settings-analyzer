55c079b8c2df19cdd218fb39f779692be4a8bf7a
bwardak <baheer.wardak06@gmail.com>
2024-05-05 02:16:59 +0100

    Added average low high and medium sensitivities

--- vct-settings/src/main/java/com/vct/settingsanalyzer/data/EmeaSensitivity.java
+++ vct-settings/src/main/java/com/vct/settingsanalyzer/data/EmeaSensitivity.java
@@ -1,65 +1,65 @@
 package com.vct.settingsanalyzer.data;
 
 public enum EmeaSensitivity {
-    DERKE("fnatic", 296),
-    ALFAJER("fnatic", 180),
-    LEO("fnatic", 164),
-    BOASTER("fnatic", 208),
-    CHRONICLE("fnatic", 200),
-    KEIKO("team liquid", 200),
-    NATS("team liquid", 392),
-    ENZO("team liquid", 251.2),
-    JAMPPI("team liquid", 253.6),
-    MISTIC("team liquid", 192),
-    MARTEEN("karmine corp", 344),
-    N4RRATE("karmine corp", 256),
-    TOMASZY("karmine corp", 252),
-    MAGNUM("karmine corp", 181.6),
-    SHIN("karmine corp", 210),
-    ARDIIS("natus vincere", 216),
-    SHAO("natus vincere", 240),
-    SUYGETSU("natus vincere", 200),
-    ANGE1("natus vincere", 240),
-    ZYPPAN("natus vincere", 200),
-    BENJYFISHY("team heretics", 226.4),
-    MINIBOO("team heretics", 232),
-    BOO("team heretics", 300),
-    RIENS("team heretics", 216),
-    WO0T("team heretics", 250),
-    SAYF("vitality", 240),
-    RUNNER("vitality", 176),
-    KICKS("vitality", 216),
-    TREXX("vitality", 180.8),
-    CENDER("vitality", 140),
-    CLOUD("giantx", 280),
-    FIT1NHO("giantx", 251.2),
-    HOODY("giantx", 251.2),
-    REDGAR("giantx", 216),
-    PURP0("giantx", 280),
-    BEYAZ("gentle mates", 195.2),
-    LOGAN("gentle mates", 140),
-    NATANK("gentle mates", 320),
-    TAKAS("gentle mates", 192),
-    WAILERS("gentle mates", 200),
-    BRAVE("bbl", 340),
-    ELITE("bbl", 256),
-    PAURA("bbl", 240),
-    REAZY("bbl", 176),
-    QUTIONERX("bbl", 171.2),
-    MRFALIN("fut esports", 264),
-    QRAXS("fut esports", 480),
-    ATAKAPTAN("fut esports", 224),
-    YETUJEY("fut esports", 560),
-    CNED("fut esports", 240)
-    ;
-
+    DERKE("fnatic", 296, "duelist"),
+    ALFAJER("fnatic", 180, "sentinel"),
+    LEO("fnatic", 164, "initiator"),
+    BOASTER("fnatic", 208, "controller"),
+    CHRONICLE("fnatic", 200, "initiator"),
+    KEIKO("team liquid", 200, "duelist"),
+    NATS("team liquid", 392, "sentinel"),
+    ENZO("team liquid", 251.2, "initiator"),
+    JAMPPI("team liquid", 253.6, "initiator"),
+    MISTIC("team liquid", 192, "controller"),
+    MARTEEN("karmine corp", 344, "duelist"),
+    N4RRATE("karmine corp", 256, "initiator"),
+    TOMASZY("karmine corp", 252, "controller"),
+    MAGNUM("karmine corp", 181.6, "sentinel/initiator"),
+    SHIN("karmine corp", 210, "controller"),
+    ARDIIS("natus vincere", 216, "duelist"),
+    SHAO("natus vincere", 240, "initiator"),
+    SUYGETSU("natus vincere", 200, "sentinel"),
+    ANGE1("natus vincere", 240, "controller"),
+    ZYPPAN("natus vincere", 200, "initiator/duelist"),
+    BENJYFISHY("team heretics", 226.4, "sentinel"),
+    MINIBOO("team heretics", 232, "duelist"),
+    BOO("team heretics", 300, "controller"),
+    RIENS("team heretics", 216, "initiator"),
+    WO0T("team heretics", 250, "initiator"),
+    SAYF("vitality", 240, "sentinel"),
+    RUNNER("vitality", 176, "duelist"),
+    KICKS("vitality", 216, "initiator"),
+    TREXX("vitality", 180.8, "initiator/controller"),
+    CENDER("vitality", 140, "controller"),
+    CLOUD("giantx", 280, "initiator"),
+    FIT1NHO("giantx", 251.2, "duelist"),
+    HOODY("giantx", 251.2, "sentinel"),
+    REDGAR("giantx", 216, "controller"),
+    PURP0("giantx", 280, "duelist"),
+    BEYAZ("gentle mates", 195.2, "initiator"),
+    LOGAN("gentle mates", 140, "sentinel"),
+    NATANK("gentle mates", 320, ""),
+    TAKAS("gentle mates", 192, "duelist"),
+    WAILERS("gentle mates", 200, "controller"),
+    BRAVE("bbl", 340, "controller"),
+    ELITE("bbl", 256, "sentinel"),
+    PAURA("bbl", 240, "initiator"),
+    REAZY("bbl", 176, "initiator"),
+    QUTIONERX("bbl", 171.2, "duelist"),
+    MRFALIN("fut esports", 264, "initiator"),
+    QRAXS("fut esports", 480, "initiator"),
+    ATAKAPTAN("fut esports", 224, "controller"),
+    YETUJEY("fut esports", 560, "sentinel"),
+    CNED("fut esports", 240, "duelist");
 
     private final String teamName;
     private final double edpi;
+    private final String role;  // new field for role
 
-    EmeaSensitivity(String teamName, double edpi) {
+    EmeaSensitivity(String teamName, double edpi, String role) {
         this.teamName = teamName;
         this.edpi = edpi;
+        this.role = role;
     }
 
     public String getTeamName() {
@@ -68,5 +68,9 @@ public enum EmeaSensitivity {
 
     public double getEdpi() {
         return edpi;
+    }
+
+    public String getRole() {
+        return role;
     }
 }
--- vct-settings/src/main/java/com/vct/settingsanalyzer/sensitivity/EmeaAverageSensitivity.java
+++ vct-settings/src/main/java/com/vct/settingsanalyzer/sensitivity/EmeaAverageSensitivity.java
@@ -1,88 +1,125 @@
 package com.vct.settingsanalyzer.sensitivity;
 
+import com.vct.settingsanalyzer.Main;
 import com.vct.settingsanalyzer.data.EmeaSensitivity;
 
 import java.util.ArrayList;
 
 public class EmeaAverageSensitivity implements SensitivityFunctionality{
-    private ArrayList<Double>  allSensitivities = new ArrayList<>();
-    private ArrayList<Double> allSensitiviesAt800Dpi = new ArrayList<>();
-    private ArrayList<Double> allSensitiviesAt1600dpi = new ArrayList<>();
+    private ArrayList<Double> allSensitivities = new ArrayList<>();
+    private ArrayList<Double> allSensitivitiesAt800Dpi  = new ArrayList<>();
+    private ArrayList<Double> allSensitivitiesAt1600dpi  = new ArrayList<>();
+    private ArrayList<Double> allHighSensitivities = new ArrayList<>();
+    private ArrayList<Double> allMidSensitivities = new ArrayList<>();
+    private ArrayList<Double> allLowSensitivities = new ArrayList<>();
+
     @Override
     public void averageSensitivity() {
         for (EmeaSensitivity sensitivity : EmeaSensitivity.values()) {
             allSensitivities.add(sensitivity.getEdpi());
         }
-        System.out.println(allSensitivities);
-    }
-
-    @Override
-    public void averageLowSensitivity() {
-
-    }
-
-    @Override
-    public void averageMediumSensitivity() {
-
-    }
-
-    @Override
-    public void averageHighSensitivity() {
-
-    }
-
-    @Override
-    public void groupLowSensitivity() {
-
-    }
-
-    @Override
-    public void groupMediumSensitivity() {
-
-    }
-
-    @Override
-    public void groupHighSensitivity() {
-
-    }
-
-    @Override
-    public void convertEdpiToSens() {
-        averageSensitivity();
-        for (Double edpi : allSensitivities) {
-            double sensAt800Dpi = edpi / 800;
-            double sensAt1600Dpi = edpi / 1600;
-
-            allSensitiviesAt800Dpi.add(sensAt800Dpi);
-            allSensitiviesAt1600dpi.add(sensAt1600Dpi);
-        }
-    }
-
-    public static void main(String[] args) {
-        EmeaAverageSensitivity sens = new EmeaAverageSensitivity();
-
-        sens.averageSensitivity();
-        sens.convertEdpiToSens();
         double allEdpi =
-                sens.allSensitivities.stream()
+                allSensitivities.stream()
                         .mapToDouble(Double::doubleValue)
                         .average()
                         .orElse(Double.NaN);
         System.out.println("Edpi: " + allEdpi);
+    }
 
+    @Override
+    public void averageLowSensitivity() {
+        groupLowSensitivity();
+        double averageLowSens =
+                allLowSensitivities.stream()
+                        .mapToDouble(Double::doubleValue)
+                        .average()
+                        .orElse(Double.NaN);
+        System.out.println("Average Low Sens: " + Math.round(averageLowSens * 1000d) / 1000d);
+    }
+
+    @Override
+    public void averageMediumSensitivity() {
+        groupMediumSensitivity();
+        double averageMidSens =
+                allMidSensitivities.stream()
+                        .mapToDouble(Double::doubleValue)
+                        .average()
+                        .orElse(Double.NaN);
+        System.out.println("Average Medium Sens: " + Math.round(averageMidSens * 1000d) / 1000d);
+    }
+
+    @Override
+    public void averageHighSensitivity() {
+        groupHighSensitivity();
+        double averageHighSens =
+                allHighSensitivities.stream()
+                        .mapToDouble(Double::doubleValue)
+                        .average()
+                        .orElse(Double.NaN);
+        System.out.println("Average High Sens: " + Math.round(averageHighSens * 1000d) / 1000d);
+    }
+
+    @Override
+    public void groupLowSensitivity() {
+        for (Double sensitivity : allSensitivitiesAt800Dpi) {
+            if (sensitivity < 0.3) {
+                allLowSensitivities.add(sensitivity);
+            }
+        }
+        System.out.println(allLowSensitivities);
+    }
+
+    @Override
+    public void groupMediumSensitivity() {
+        for (Double sensitivity : allSensitivitiesAt800Dpi) {
+            if (sensitivity > 0.3 && sensitivity < 0.4) {
+                allMidSensitivities.add(sensitivity);
+            }
+        }
+        System.out.println(allMidSensitivities);
+    }
+
+    @Override
+    public void groupHighSensitivity() {
+        for (Double sensitivity : allSensitivitiesAt800Dpi) {
+            if (sensitivity > 0.4) {
+                allHighSensitivities.add(sensitivity);
+            }
+        }
+        System.out.println(allHighSensitivities);
+    }
+
+    @Override
+    public void convertEdpiToSens() {
+        for (Double edpi : allSensitivities) {
+            double sensAt800Dpi = Math.round((edpi / 800) * 1000d) / 1000d;
+            double sensAt1600Dpi = Math.round((edpi / 1600) * 1000d) / 1000d;
+
+            allSensitivitiesAt800Dpi .add(sensAt800Dpi);
+            allSensitivitiesAt1600dpi.add(sensAt1600Dpi);
+        }
         double all800dpi =
-                sens.allSensitiviesAt800Dpi.stream()
+                allSensitivitiesAt800Dpi.stream()
                         .mapToDouble(Double::doubleValue)
                         .average()
                         .orElse(Double.NaN);
         System.out.println("800 Dpi: " + Math.round(all800dpi * 1000d) / 1000d);
 
         double all1600dpi =
-                sens.allSensitiviesAt1600dpi.stream()
+                allSensitivitiesAt1600dpi.stream()
                         .mapToDouble(Double::doubleValue)
                         .average()
                         .orElse(Double.NaN);
         System.out.println("1600 Dpi: " + Math.round(all1600dpi * 1000d) / 1000d);
+    }
 
+    public static void main(String[] args) {
+
+        EmeaAverageSensitivity sens = new EmeaAverageSensitivity();
+        sens.averageSensitivity();
+        sens.convertEdpiToSens();
+        sens.averageLowSensitivity();
+        sens.averageMediumSensitivity();
+        sens.averageHighSensitivity();
     }
 }
